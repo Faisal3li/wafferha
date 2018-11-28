e2c851744072524453dd2de047126c39e4ed80c8
rawan-1 <rawan.cs18@gmail.com>
2018-11-28 21:03:07 +0300

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_085914) do
+ActiveRecord::Schema.define(version: 2018_11_28_144005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2018_11_28_085914) do
     t.boolean "used"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.integer "company_id"
   end
 
   create_table "offers_users", id: false, force: :cascade do |t|
