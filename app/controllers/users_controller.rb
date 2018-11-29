0f970b8e9790af51f502a957917ec5ffced3824d
rawan-1 <rawan.cs18@gmail.com>
2018-11-29 10:45:10 +0300

    Companies form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
-
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find_by(id: params[:id])
   end
 end
