dfc7f94091d861883763561ba4b888aaf2b9ffa2
MashaelAlmosallam <mashaelmosallam@gmail.com>
2018-11-29 16:54:27 +0300

    login/out & nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root "pages#home"
   #root "offers#index"
+  devise_for :users
   resources :companies, :offers, :users
   resources :categories, only: [:show]
   get "/offers/:id/new", to: "offers#new"
-
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
