7c736afca31e08e3e78165b69d1681fbfad5aa90
MashaelAlmosallam <mashaelmosallam@gmail.com>
2018-11-29 10:27:45 +0300

    company controller Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'offers/index'
-  get 'offers/show'
-  get 'offers/edit'
-  get 'offers/new'
-  get 'categories/show'
   resources :companies, :offers, :users
   resources :categories, only: [:show]
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_144005) do
+ActiveRecord::Schema.define(version: 2018_11_29_072051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_144005) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "offers", force: :cascade do |t|
@@ -57,6 +58,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_144005) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
