dcdd0a90de6836a08cddfe2d99303d22804fcabb
razan <almusarazan@gmail.com>
2018-12-01 15:09:58 +0300

    4

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -17,13 +17,13 @@ class OffersController < ApplicationController
   end
 
   def create
-    @offer = Offer.new(offer_params)
+    @offer = Offer.create(offer_params)
     redirect_to @offer
   end
 
   private
 
   def offer_params
-    params.require(:offer).permit(:offer_Description, :image, :expired_Date, :start_Date, :coLocation, :view_Count, :coupon, :premium, :used, company_ids: [])
+    params.require(:offer).permit(:offer_Description, :image, :expired_Date, :start_Date, :coLocation, :view_Count, :coupon, :premium, :used, company_ids:)
   end
 end
