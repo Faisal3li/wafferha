1e9d13cec097ce0ccb5027f0059484c0e76b665d
rawan-1 <rawan.cs18@gmail.com>
2018-12-03 14:06:17 +0300

    1

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.3'
 
-gem 'devise'
 gem 'geocoder'
+gem 'devise'
 
 gem 'cloudinary'
 gem 'rest-client', '~> 2.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_070028) do
+ActiveRecord::Schema.define(version: 2018_12_03_071729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,15 @@ ActiveRecord::Schema.define(version: 2018_12_02_070028) do
   create_table "offers_users", id: false, force: :cascade do |t|
     t.bigint "offer_id", null: false
     t.bigint "user_id", null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
