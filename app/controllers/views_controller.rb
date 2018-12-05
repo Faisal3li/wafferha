2618374941f67c7bf041ee6ed717875a19dab6cb
rawan-1 <rawan.cs18@gmail.com>
2018-12-05 09:43:22 +0300

    Views Counter

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,8 +1,9 @@
 class ViewsController < ApplicationController
-    skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def create
     view = View.create(offer_id: params["offer_id"], user_id: current_user.id)
-    render json: view
+    offer = Offer.find_by(id: params["offer_id"])
+    render json: offer.views.length
   end
 end
